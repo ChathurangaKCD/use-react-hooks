a5c1456be7a681a5f7da705b83da40cd728eaff6
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2018-12-24 01:14:16 +0530

    set animation states

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0);
+  const [isClockWise, setIsClockWise] = useState(true);
+  const [changeEnabled, setEnabled] = useState(false);
+  const [isPaused, setPaused] = useState(false);
+  useEffect(
+    () => {
+      if (!isPaused && changeEnabled) {
+        const id = setTimeout(() => {
+          setIsClockWise(!isClockWise);
+        }, 2000);
+        return () => {
+          clearTimeout(id);
+        };
+      }
+    },
+    [changeEnabled, isClockWise, isPaused]
+  );
+  const imgClass = `App-logo${isClockWise ? "" : " reverse"}${
+    isPaused ? " paused" : ""
+  }`;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className={imgClass} alt="logo" />
+        <p>Count {count} </p>
+        <p>Direction: {isClockWise ? "" : " counter"}clockwise</p>
+        <p>Switch Direction : {changeEnabled ? "enabled" : "disabled"} </p>
+        <button onClick={() => setEnabled(!changeEnabled)}>
+          <strong>rotate {changeEnabled ? "" : "<<<"} >>> </strong>
+        </button>
+        <p>Animation state: {isPaused ? "paused" : "playing"}</p>
+        <button onClick={() => setPaused(!isPaused)}>
+          <strong> {isPaused ? "PLAY" : "PAUSE"} </strong>
+        </button>
+      </header>
+      <button onClick={() => setCount(count + 1)}>(+1)</button>
+      <button onClick={() => setCount(count - 1)}>(-1)</button>
+    </div>
+  );
 }
 
 export default App;
